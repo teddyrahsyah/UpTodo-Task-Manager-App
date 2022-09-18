00b23b892d56876e94aef50ddb3e6e9eeeff6e56
teddyrahsyah212 <teddyrahsyah@gmail.com>
2022-09-18 22:51:50 +0700

    splash page UI

---
+++ lib/shared/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = Color(0xff8687E7);
+Color kBackgroundColor = Color(0xff121212);
+Color kFormColor = Color(0xff1D1D1D);
+Color kFullWhiteColor = Color(0xffFFFFFF);
+Color kWhiteColor = Color(0xdeFFFFFF);
+Color kGrayColor = Color(0xff979797);
+Color kModalColor = Color(0xffAFAFAF);
+
+TextStyle whiteTextStyle = GoogleFonts.lato(color: kWhiteColor);
+
+TextStyle fullWhiteTextStyle = GoogleFonts.lato(color: kFullWhiteColor);
+
+TextStyle grayTextStyle = GoogleFonts.lato(color: kGrayColor);
+
+FontWeight regular = FontWeight.w400;
+FontWeight bold = FontWeight.w700;
---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:up_todo_app/shared/theme.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  // @override
+  // void initState() {
+  //   Timer(const Duration(seconds: 3), () {
+  //     Navigator.pushNamed(context, "/intro");
+  //   });
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/icons/icon_logo.png",
+              width: 113,
+              height: 113,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "UpTodo",
+              style: fullWhiteTextStyle.copyWith(fontSize: 40, fontWeight: bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
